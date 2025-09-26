b2cf6568e140a077bb0bdd539234d50d74a30e95
Lucas Sabino <sabino.lucas2016@gmail.com>
2025-09-25 21:32:33 -0300

    ING-37 db

---
+++ app/db/models.py
@@ -0,0 +1,109 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    Date,
+    ForeignKey,
+    Enum,
+    UniqueConstraint
+)
+from sqlalchemy.orm import relationship
+from .database import Base
+import enum
+
+
+#Checks / Enums
+class CardType(str, enum.Enum):
+    EVENT = "EVENT"
+    SECRET = "SECRET"
+    INSTANT = "INSTANT"
+    DEVIUOS = "DEVIUOS"
+    DETECTIVE = "DETECTIVE"
+    END = "END"
+
+
+class RoomStatus(str, enum.Enum):
+    WAITING = "WAITING"
+    INGAME = "INGAME"
+    FINISH = "FINISH"
+
+
+class CardState(str, enum.Enum):
+    DECK = "DECK"
+    SELECT = "SELECT"
+    DISCARD = "DISCARD"
+    SECRET_SET = "SECRET_SET"
+    DETECTIVE_SET = "DETECTIVE_SET"
+    HAND = "HAND"
+
+
+# ------------------------
+# TABLES
+# ------------------------
+class Game(Base):
+    __tablename__ = "game"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    player_turn_id = Column(Integer)
+
+    rooms = relationship("Room", back_populates="game")
+    cards = relationship("CardsXGame", back_populates="game")
+
+
+class Card(Base):
+    __tablename__ = "card"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(200), nullable=False)
+    description = Column(String(500), nullable=False)
+    type = Column(Enum(CardType), nullable=False)
+    img_src = Column(String(500), nullable=False)
+
+    games = relationship("CardsXGame", back_populates="card")
+
+
+class Room(Base):
+    __tablename__ = "room"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(200), nullable=False)
+    player_qty = Column(Integer, nullable=False)
+    password = Column(String(500))
+    status = Column(Enum(RoomStatus), nullable=False)
+    id_game = Column(Integer, ForeignKey("game.id"))
+
+    game = relationship("Game", back_populates="rooms")
+    players = relationship("Player", back_populates="room")
+
+
+class Player(Base):
+    __tablename__ = "player"
+    __table_args__ = (
+        UniqueConstraint("name", "avatar_src", name="uq_player_name_avatar"),
+    )
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(200), nullable=False)
+    avatar_src = Column(String(500), nullable=False)
+    birthdate = Column(Date, nullable=False)
+    id_room = Column(Integer, ForeignKey("room.id"))
+    is_host = Column(Boolean, default=False)
+    order = Column(Integer)
+
+    room = relationship("Room", back_populates="players")
+    cards = relationship("CardsXGame", back_populates="player")
+
+
+class CardsXGame(Base):
+    __tablename__ = "cardsXgame"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    id_game = Column(Integer, ForeignKey("game.id"), nullable=False)
+    id_card = Column(Integer, ForeignKey("card.id"), nullable=False)
+    is_in = Column(Enum(CardState), nullable=False)
+    position = Column(Integer, nullable=False)
+    player_id = Column(Integer, ForeignKey("player.id"))
+
+    game = relationship("Game", back_populates="cards")
+    card = relationship("Card", back_populates="games")
+    player = relationship("Player", back_populates="cards")
