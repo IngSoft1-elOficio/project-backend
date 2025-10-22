ae470aec3e8ea8c6fb51c998ff50ff62640cee1d
mariajosearandaviana <maria.aranda.viana@mi.unc.edu.ar>
2025-10-22 13:39:14 -0300

    ING-86 add error handling

--- app/schemas/event_schema.py
+++ app/schemas/event_schema.py
@@ -2,17 +2,13 @@ from pydantic import BaseModel
 
 
 # And then was one more (Step 1)
-class SecretSummary(BaseModel):
-    secret_id: int
-    player_id: int
-    
 class OneMoreStartRequest(BaseModel):
     card_id: int # esto ya tiene el id del jugador al que hay q sacarle el secreto
 
 
 class OneMoreStartResponse(BaseModel):
     action_id: int
-    available_secrets: list[SecretSummary]
+    available_secrets: list[int]
 
 # And then was one more (Step 2)
 class OneMoreSecondRequest(BaseModel):
