8e74efb14eb7cef2bcf3e1ccbc67047477385b6e
Felipe Tomas Pagani <66705993+the-other-flaneur@users.noreply.github.com>
2025-11-05 13:48:55 -0300

    ING-118 Creates route and handler

--- app/main.py
+++ app/main.py
@@ -77,6 +77,8 @@ from app.routes import another_victim
 app.include_router(another_victim.router)
 from app.routes import cards_off_the_table
 app.include_router(cards_off_the_table.router)
+from app.routes import add_to_set
+app.include_router(add_to_set.router)
 
 # Aplicación ASGI con Socket.IO
 socket_app = socketio.ASGIApp(sio, app)
